9e6c7e7c7cf19623b966b232bf8d29b985316997
Joseph Miklus <Joseph Miklus>
2016-07-24 17:09:06 -0400

    Submission

--- MethodHomework/Program.cs
+++ MethodHomework/Program.cs
@@ -22,10 +22,12 @@ namespace MethodHomework
                 Console.WriteLine("2.) Birth Month");
                 Console.WriteLine("3.) Name Capitalizer");
                 Console.WriteLine("4.) Zero Remover");
-                Console.WriteLine("5.) Run them all in order\n");
+                Console.WriteLine("5.) Run them all in order");
+                Console.WriteLine("6.) Quit\n");
                 Console.Write("Enter your selection: ");
                 string choice = Console.ReadLine();
 
+                //String reverser
                 if (choice == "1" || choice == "5")
                 {
                     Console.Clear();
@@ -36,6 +38,8 @@ namespace MethodHomework
                     Console.Write("\nHit any key to continue.");
                     Console.ReadKey();
                 }
+
+                //Birth month
                 if (choice == "2" || choice == "5")
                 {
                     Console.Clear();
@@ -48,6 +52,8 @@ namespace MethodHomework
                     Console.Write("\nHit any key to continue.");
                     Console.ReadKey();
                 }
+
+                //Name capitalizer
                 if (choice == "3" || choice == "5")
                 {
                     Console.Clear();
@@ -57,6 +63,31 @@ namespace MethodHomework
                     string last = Console.ReadLine();
                     string full = NameCapitalizer(first, last);
                     Console.WriteLine("\nFull name: " + full);
+                    Console.Write("\nHit any key to continue.");
+                    Console.ReadKey();
+                }
+
+                //Zero remover
+                if (choice == "4" || choice == "5")
+                {
+                    Console.Clear();
+                    Console.Write("Enter the string to check for zeroes: ");
+                    string zeroString = Console.ReadLine();
+                    ZeroRemover(zeroString);
+                    Console.Write("\nHit any key to continue.");
+                    Console.ReadKey();
+                }
+
+                if (choice == "6")
+                {
+                    loop = false;
+                }
+
+                if (choice != "1" && choice != "2" && choice != "3"
+                    && choice != "4" && choice != "5" && choice != "6")
+                {
+                    Console.Write("\nInvalid selection. Hit any key to continue.");
+                    Console.ReadKey();
                 }
 
             }
@@ -68,7 +99,11 @@ namespace MethodHomework
             
             for (int x = sentence.Length - 1; x >= 0; x--)
             {
-                Console.Write(sentence[x] + " ");
+                Console.Write(sentence[x]);
+                if (x > 0)
+                {
+                    Console.Write(" ");
+                }
             }
 
             Console.WriteLine();
@@ -86,18 +121,31 @@ namespace MethodHomework
 
         static string NameCapitalizer(string f, string l)
         {
-            for (int x=1; x<f.Length; x++)
-            {
-
-            }
-
-            string name = "";
+            string name = f.Substring(0, 1).ToUpper() + f.Substring(1) + " " + l.Substring(0, 1).ToUpper() + l.Substring(1);
             return name;
         }
 
         static void ZeroRemover(string s)
         {
+            bool flag = false;
 
+            for (int x = 0; x < s.Length; x++)
+            {
+                if (s.Substring(x,1) == "0")
+                {
+                    s = s.Remove(x, 1);
+                    flag = true;
+                }
+            }
+
+            if (flag == true)
+            {
+                Console.WriteLine("\n" + s);
+            }
+            else
+            {
+                Console.WriteLine("This string is okay!");
+            }
         }
     }
 }
